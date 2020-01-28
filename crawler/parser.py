2a8b9836bdfeb7eb882a0eda9fbf507f86780484
rcy <rcycyzh@163.com>
2020-01-28 14:06:01 +0800

    Add some provinces #1

--- crawler/parser.py
+++ crawler/parser.py
@@ -7,27 +7,27 @@ Here define parser functions to parse url and title
 from urllib.parse import urljoin
 
 
-def title_in_attr_href_in_attr(host, node):
+def title_in_attr_href_in_attr(url, node):
     """
     <a href="/related/url" title="the title">the shown title</a>
     """
     return {
         'title': node.get('title'),
-        'url': urljoin(host, node.get('href')),
+        'url': urljoin(url, node.get('href')),
     }
 
 
-def title_in_text_href_in_attr(host, node):
+def title_in_text_href_in_attr(url, node):
     """
     <a href="/related_url">the title</a>
     """
     return {
         'title': node.text.replace('\n', ' ').strip(),
-        'url': urljoin(host, node.get('href')),
+        'url': urljoin(url, node.get('href')),
     }
 
 
-def chongqing_parser(host, node):
+def chongqing_parser(url, node):
     """
     <a href="/related_url">
         <span>date</span>
@@ -36,5 +36,28 @@ def chongqing_parser(host, node):
     """
     return {
         'title': node.find('span').find_next_sibling().get('title'),
-        'url': urljoin(host, node.get('href')),
+        'url': urljoin(url, node.get('href')),
+    }
+
+
+def henan_parser(url, node):
+    """
+    It seems that Henan has two list respectively ordered by time,
+    so I should find the second list's first news
+    """
+    import re
+    pattern = re.compile(r'\d{4}-\d{2}-\d{2}')
+    result = node
+    last_date = pattern.search(node.text).group()
+    while node.find_next_sibling():
+        node = node.find_next_sibling()
+        node_date = pattern.search(node.text).group()
+        if node_date > last_date:
+            result = node
+            break
+        last_date = node_date
+    node = node.find('a')
+    return {
+        'title': node.text,
+        'url': urljoin(url, node.get('href'))
     }

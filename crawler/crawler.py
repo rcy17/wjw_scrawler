05837b46c13d2d8020c01e68eda73dced9be66a2
rcy <rcycyzh@163.com>
2020-02-05 14:37:26 +0800

    Send province report to default email

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -89,7 +89,7 @@ class Crawler:
         # If this is not recorded, save and report it
         if result != buffer:
             name = self.others['Chinese_name']
-            self.manager.add_message(name, self.others.get('province', name), result)
+            self.manager.add_message(name, self.others.get('province', 'default'), result)
 
     def get_task(self, session):
         return asyncio.ensure_future(self.run(session))
